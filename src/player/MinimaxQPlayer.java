4800cf7c27ade3962978bd7c4f782ab3607c56c5
Jan <jan-lucas@hetnet.nl>
2017-03-24 15:29:15 +0100

    implementation minimaxQ started

--- src/player/MinimaxQPlayer.java
+++ src/player/MinimaxQPlayer.java
@@ -1,6 +1,7 @@
 package player;
 
 import java.util.Map;
+import java.util.Random;
 
 import exploration.ExplorationStrategy;
 import main.Action;
@@ -18,22 +19,79 @@ import scpsolver.problems.LinearProgram;
 public class MinimaxQPlayer implements Player {
 	private boolean player;
 	private State s;
+	private Action a;
 
 	private Map<Triple<State, Action, Action>, Double> qValues;
 	private Map<State, double[]> pi;
+	private Map<State, Double> V;
+	private ExplorationStrategy strat;
+
+	double alpha = 1.0;
+	double gamma;
+	double decay;
 
 	public MinimaxQPlayer(boolean player, double discountFactor, double decay, ExplorationStrategy es) {
+		gamma = discountFactor;
+		decay = decay
+//		For all s in S, a in A, and o in O,
+//				Let Q[s,a,o] := 1
+//		For all s in S,
+//				Let V[s] := 1
+//		For all s in S, a in A,
+//		Let pi[s,a] := 1/|A|
+//				Let alpha := 1.0
+
 		this.player = player;
+		strat = es;
 	}
 
 	@Override
 	public Action chooseAction(State state) {
-		return new RandomPlayer(this.player).chooseAction(state);
+		s = state;
+		if(Math.random() < explor){
+			a =  strat.selectAction(state, player);
+		}
+		else{
+			double[] probabiilities = pi.get(state);
+			Action[] actions = Action.values();
+			double sum = 0.0;
+			double r = Math.random();
+
+			for (int i = 0; i < probabiilities.length; i++) {
+				sum = sum + probabiilities[i];
+				if (r <= sum) {
+					a = actions[i];
+					break;
+				}
+			}
+		}
+		// With probability explor, return an action uniformly at random.
+		// Otherwise, if current state is s,
+		// Return action a with probability pi[s,a]
+		return a;
 	}
 
 	@Override
 	public void receiveReward(double reward, State newState, Action opponentAction) {
+//		After receiving reward rew for moving from state s to s’
+//		via action a and opponent’s action o,
+//				Let Q[s,a,o] := (1-alpha) * Q[s,a,o] + alpha * (rew + gamma * V[s’])
+		Triple sao = new Triple<State, Action, Action>(newState, a, opponentAction);
+		double newq = (1 - alpha) * qValues.get(sao) + alpha * (reward + gamma * V.get(newState));
+		qValues.put(sao, newq);
+//		Use linear programming to find pi[s,.] such that:
+//		pi[s,.] := argmaxfpi’[s,.], minfo’, sumfa’, pi[s,a’] * Q[s,a’,o’]ggg
+		linearProgramming();
 
+//		Let V[s] := minfo’, sumfa’, pi[s,a’] * Q[s,a’,o’]gg
+		V.put(s, getNewV());
+
+//		Let alpha := alpha * decay
+		alpha *= decay;
+	}
+
+	private double getNewV(){
+//		min{o’, sum{a’, pi[s,a’] * Q[s,a’,o’] }}
 	}
 
 	public void linearProgramming() {
